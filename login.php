d76bfa4dc61f19b2fdf1ae2bd65280f70f6cf8a6
NikEurope <nik.europe@gmail.com>
2016-02-18 10:22:38 +0100

    2

---
+++ login.php
@@ -0,0 +1,51 @@
+<meta charset="utf-8" />
+<?php include('includes/config.php');
+
+if(isset($_POST['envoyer'])) {
+  if(isset($_POST['pseudo']) AND !empty($_POST['pseudo'])
+  AND isset($_POST['motdepasse']) AND !empty($_POST['motdepasse']))
+  {
+  // Tout les champs ont ete remplis
+  $pseudo = mysql_escape_string($_POST['pseudo']);
+  $motdepasse = mysql_escape_string(md5($_POST['motdepasse']));
+
+  $req1 = mysql_query('SELECT * FROM membres WHERE pseudo = "'.$pseudo.'"');
+  $info_membre = mysql_fetch_array($req1);
+
+  if(isset($info_membre['pseudo'])) {
+
+  if($motdepasse == $info_membre['motdepasse'])
+  {
+  // mdp bon
+  $_SESSION['pseudo'] = $pseudo;
+  $succes = 'Connexion reussie !';
+
+  }
+  else
+  {
+  $erreur = 'Mot de passe est incorrect.';
+  }
+
+  }
+  else
+  {
+  $erreur = 'Le pseudo n\'existe pas. ';
+  }
+  }
+  else
+  {
+  $erreur='Tout les champs sont obligatoires !';
+  }
+  }
+?>
+<h1>Creer un compte</h1>
+<br />
+<?php if(isset($erreur)) {echo ' Erreur : '.$erreur;}?>
+<?php if(isset($succes)) {echo $succes;}?>
+<hr />
+<br />
+<form action="login.php" method="post">
+  Pseudo : <input type="text" name="pseudo" value="<?php if(isset($_POST['pseudo'])) {echo $_POST['pseudo'];}?>"/><br /><br />
+  Mot de passe : <input type="password" name="motdepasse" value="<?php if(isset($_POST['motdepasse'])) {echo $_POST['motdepasse'];}?>"/><br /><br />
+  <input type="submit" name="envoyer" value ="Se connecter !"/>
+</form>
---
+++ register.php
@@ -0,0 +1,67 @@
+<meta charset="utf-8" />
+<?php include('includes/config.php');
+
+if(isset($_POST['envoyer'])) {
+  if(isset($_POST['pseudo']) AND !empty($_POST['pseudo'])
+  AND isset($_POST['motdepasse']) AND !empty($_POST['motdepasse'])
+  AND isset($_POST['motdepasse2']) AND !empty($_POST['motdepasse2'])
+  AND isset($_POST['email']) AND !empty($_POST['email']))
+  {
+  // Tout les champs ont ete remplis
+  $pseudo = mysql_escape_string($_POST['pseudo']);
+  $motdepasse = mysql_escape_string (md5($_POST['motdepasse']));
+  $motdepasse2 = mysql_escape_string (md5($_POST['motdepasse2']));
+  $email = mysql_escape_string($_POST['email']);
+
+  $req2 = mysql_query('SELECT pseudo FROME membres WHERE pseudo = "'.$pseudo.'"');
+  $info_membre = mysql_fetch_array($req2);
+
+  if(!isset($info_membre['pseudo'])) {
+
+  $longueur_pseudo = strlen($pseudo);
+  if($longueur_pseudo <= 30)
+  {
+  // Pseudo respecte le format
+  if($motdepasse == $motdepasse2)
+  {
+    //Les mots de passe sont identique
+  mysql_query('INSERT INTO membres VALUES("", "'.$pseudo.'", "'.$motdepasse.'", "'.$email.'")');
+  $succes = 'Le compte a été crée ! Vous pouvez vous connecter en cliquant <a href="login.php"> ici >>> </a>';
+  }
+  else
+  {
+  $erreur = 'La confirmation ne coresponde pas a mot de passe';
+  }
+
+  }
+  else
+  {
+  $erreur = 'Pseudo est tres long (max 30)';
+  }
+  }
+  else
+  {
+  $erreur = 'Pseudo existe deja';
+  }
+  }
+  else
+  {
+  $erreur='Tout les champs sont obligatoires !';
+  }
+
+}
+?>
+
+<h1>Creer un compte</h1>
+<br />
+<?php if(isset($erreur)) {echo ' Erreur : '.$erreur;}?>
+<?php if(isset($succes)) {echo $succes;}?>
+<hr />
+<br />
+<form action="register.php" method="post">
+  Pseudo : <input type="text" name="pseudo" value="<?php if(isset($_POST['pseudo'])) {echo $_POST['pseudo'];}?>"/><br /><br />
+  Mot de passe : <input type="password" name="motdepasse" value="<?php if(isset($_POST['motdepasse'])) {echo $_POST['motdepasse'];}?>"/><br /><br />
+  Mot de passe confirmation : <input type="password" name="motdepasse2" value="<?php if(isset($_POST['motdepasse2'])) {echo $_POST['motdepasse2'];}?>"/><br /><br />
+  Mail : <input type="text" name="email" value="<?php if(isset($_POST['email'])) {echo $_POST['email'];}?>"/><br /><br />
+  <input type="submit" name="envoyer" value ="S'inscrire !"/><br />
+</form>

70f7f25d4d2b7f13c7af812893698528409e9ad6
NikEurope <nik.europe@gmail.com>
2016-02-18 11:47:15 +0100

    4

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!-- VIDEO https://www.youtube.com/watch?v=bv3D3U5d2ck&index=15&list=PL8407690672542830 -->
+<meta charset="utf-8" />
+<?php include('includes/config.php'); ?>
+
+<?php
+if(isset($_SESSION['pseudo'])) { ?>
+
+<p>
+Vous etes connecté, <?php echo $_SESSION['pseudo']; ?> . <br /><br />
+<a href="logout.php"/>Se déconnecter</a><br /></p>
+
+<?php  } else { ?>
+
+<br />
+<p>
+Vous n'etes pas connecté, visiteur.
+</p>
+
+<p><a href="login.php"/>Se connecter</a><br /><br />
+<a href="register.php" />S'inscrire</a>
+</p>
+
+
+<?php  }  ?>
